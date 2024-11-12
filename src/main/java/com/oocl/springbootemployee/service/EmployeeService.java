2bd2d327a20f5da23fd94b9cab98812fbdf7041a
Jiancheng LI <jiancheng.li@thoughtworks.com>
2024-11-12 17:44:42 +0800

    [SpringBoot][3Layer] refactor, using IEmployeeRepository interface employee service

--- src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/oocl/springbootemployee/service/EmployeeService.java
@@ -1,15 +1,15 @@
 package com.oocl.springbootemployee.service;
 
 import com.oocl.springbootemployee.model.Employee;
-import com.oocl.springbootemployee.repository.EmployeeRepository;
+import com.oocl.springbootemployee.repository.IEmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
-    private final EmployeeRepository employeeRepository;
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    private final IEmployeeRepository employeeRepository;
+    public EmployeeService(IEmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
     }
 
