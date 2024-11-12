19a868cdec5fa9f7d1eca85f164e82909cda72f7
Jiancheng LI <jiancheng.li@thoughtworks.com>
2024-11-12 19:03:58 +0800

    [SpringBoot][3Layer] demo, create a mocked employee repository to explain waht is test double

--- src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
@@ -1,8 +1,9 @@
 package com.oocl.springbootemployee.service;
 
 import com.oocl.springbootemployee.model.Employee;
-import com.oocl.springbootemployee.repository.EmployeeRepository;
 import com.oocl.springbootemployee.model.Gender;
+import com.oocl.springbootemployee.repository.EmployeeRepository;
+import com.oocl.springbootemployee.repository.MockedEmployeeRepository;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -13,17 +14,16 @@ class EmployeeServiceTest {
     @Test
     void should_return_the_given_employees_when_getAllEmployees() {
         //given
-        EmployeeRepository employeeRepository = new EmployeeRepository();
-        Employee lucy = new Employee(6, "Lucy", 18, Gender.FEMALE, 8000.0);
-        employeeRepository.addEmployee(lucy);
+        MockedEmployeeRepository employeeRepository = new MockedEmployeeRepository();
+        employeeRepository.whenGetAllReturn(List.of(new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0)));
         EmployeeService employeeService = new EmployeeService(employeeRepository);
 
         //when
         List<Employee> allEmployees = employeeService.getAllEmployees();
 
         //then
-        assertEquals(6, allEmployees.size());
-        assertEquals("Lucy", allEmployees.get(5).getName());
+        assertEquals(1, allEmployees.size());
+        assertEquals("Lucy", allEmployees.get(0).getName());
     }
 
     @Test
