5627618056af12119a3c8f84d4142add5c8bc9d2
Jiancheng LI <jiancheng.li@thoughtworks.com>
2024-11-12 19:32:38 +0800

    [SpringBoot][3Layer] refactor, using mockito to mock employee repository

--- src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/oocl/springbootemployee/service/EmployeeServiceTest.java
@@ -3,20 +3,22 @@ package com.oocl.springbootemployee.service;
 import com.oocl.springbootemployee.model.Employee;
 import com.oocl.springbootemployee.model.Gender;
 import com.oocl.springbootemployee.repository.EmployeeRepository;
-import com.oocl.springbootemployee.repository.MockedEmployeeRepository;
+import com.oocl.springbootemployee.repository.IEmployeeRepository;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class EmployeeServiceTest {
     @Test
     void should_return_the_given_employees_when_getAllEmployees() {
         //given
-        MockedEmployeeRepository employeeRepository = new MockedEmployeeRepository();
-        employeeRepository.whenGetAllReturn(List.of(new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0)));
-        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        IEmployeeRepository mockedEmployeeRepository = mock(IEmployeeRepository.class);
+        when(mockedEmployeeRepository.getAll()).thenReturn(List.of(new Employee(1, "Lucy", 18, Gender.FEMALE, 8000.0)));
+        EmployeeService employeeService = new EmployeeService(mockedEmployeeRepository);
 
         //when
         List<Employee> allEmployees = employeeService.getAllEmployees();
